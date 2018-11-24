d6e3b3d2c2ae5cb2311b114e3841b88381390f1b
TemirovB <42097456+TemirovB@users.noreply.github.com>
2018-11-24 19:44:31 +0600

    Add files via upload

---
+++ Sound Vision newest/Sound Vision/ViewController2.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController2.swift
+//  SoundVision
+//
+//  Created by Bakhtiar Temirov on 11/24/18.
+//  Copyright © 2018 Mitrevski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        
+
+        
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
